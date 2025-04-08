85761705c33dbe93966a2132fdaec9217e0851ec
desiirenav <desiirenav@hotmail.com>
2025-04-07 23:19:45 -0400

    hyprpanel.nix midway modification

--- homeManagerModules/hyprpanel.nix
+++ homeManagerModules/hyprpanel.nix
@@ -16,25 +16,34 @@
     overwrite.enable = true;
     overlay.enable = true;
     settings = {
-      bar.launcher.icon = "󱄅";
+      bar = {
+        launcher.icon = "󱄅";
+        clock = {
+          format = "%a %b %d  %I:%M %p";
+        };
+        network = {
+          label = false;
+        };
+        notifications = {
+          show_total = true;
+          hideCountWhenZero = false;
+        };
+      };
       layout = {
         "bar.layouts" = {
           "*" = {
             "left" = [
               "dashboard"
-              "workspaces"
-              "windowtitle"
             ];
             "middle" = [
-              "media"
+              "workspaces"
             ];
             "right" = [
               "volume"
+              "network"
               "battery"
               "clock"
-              "network"
               "systray"
-              "notifications"
             ];
           };
         };
@@ -44,6 +53,16 @@
         tooltip.scaling = 70;
         notification.scaling = 70;
         bar = {
+          margin_bottom = "0em";
+          margin_sides = "16em";
+          margin_top = "0.5em";
+          shadow = "1px 1px 12px 1px #000000";
+          shadowMargins = "0px 1px 12px 1px";
+          border_radius = "1.4em";
+          floating = true;
+          buttons = {
+            background_opacity = 0;
+          };
           scaling = 70;
           menus = {
             popover.scaling = 70;
