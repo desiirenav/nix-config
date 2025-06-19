ca06462024634e4ad7425cca26ff7d9a6d944a9b
desiirenav <desiirenav@hotmail.com>
2025-06-19 14:57:52 -0400

    new wal

--- homeManagerModules/niri/settings.nix
+++ homeManagerModules/niri/settings.nix
@@ -5,7 +5,7 @@
   lib,
   ...
 }: let
-  wallpaper = "${./../stylix/fp.png}";
+  wallpaper = "${./../stylix/doodad.png}";
   colors = config.lib.stylix.colors.withHashtag;
 in {
   programs.niri = {
--- nixosModules/stylix/stylix.nix
+++ nixosModules/stylix/stylix.nix
@@ -11,11 +11,8 @@
   stylix = {
     autoEnable = true;
     enable = true;
-    image = ./fp.png;
+    image = ./doodad.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
-    cursor = {
-      size = 22;
-    };
     fonts = {
       serif = {
         package = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
