78b77c4fde839769008378de47dad74f7b99bc0b
desiirenav <desiirenav@hotmail.com>
2025-06-23 22:12:14 -0400

    nixvim config

--- nixosModules/packages/nixvim.nix
+++ nixosModules/packages/nixvim.nix
@@ -7,9 +7,24 @@
 }: {
   imports = [inputs.nixvim.nixosModules.nixvim];
 
-  colorschemes.ayu.enable = true;
-
-  plugins = {
-    lualine.enable = true;
+  programs.nixvim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    globalOpts = {
+      number = true;
+      relativenumber = true;
+    };
+    colorschemes.ayu.enable = true;
+    plugins = {
+      lualine.enable = true;
+      web-devicons.enable = true;
+      telescope.enable = true;
+      treesitter.enable = true;
+      typst-vim.enable = true;
+      lsp = {
+        enable = true;
+      };
+    };
   };
 }
