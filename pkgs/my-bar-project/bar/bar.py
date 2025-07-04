be0105aaf60e04a86b80a6562e6777ce6f121bc2
desiirenav <desiirenav@hotmail.com>
2025-07-03 23:48:48 -0400

    update bar

--- pkgs/my-bar-project/bar/bar.py
+++ pkgs/my-bar-project/bar/bar.py
@@ -1,20 +1,104 @@
-import fabric
+import psutil
 from fabric import Application
 from fabric.widgets.box import Box
+from fabric.widgets.label import Label
+from fabric.widgets.overlay import Overlay
+from fabric.widgets.eventbox import EventBox
 from fabric.widgets.datetime import DateTime
 from fabric.widgets.centerbox import CenterBox
+from fabric.system_tray.widgets import SystemTray
+from fabric.widgets.circularprogressbar import CircularProgressBar
 from fabric.widgets.wayland import WaylandWindow as Window
+from fabric.utils import (
+    FormattedString,
+    bulk_replace,
+    invoke_repeater,
+    get_relative_path,
+)
+
+AUDIO_WIDGET = True
+
+if AUDIO_WIDGET is True:
+    try:
+        from fabric.audio.service import Audio
+    except Exception as e:
+        print(e)
+        AUDIO_WIDGET = False
+
+
+class VolumeWidget(Box):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.audio = Audio()
+
+        self.progress_bar = CircularProgressBar(
+            name="volume-progress-bar", pie=True, size=24
+        )
+
+        self.event_box = EventBox(
+            events="scroll",
+            child=Overlay(
+                child=self.progress_bar,
+                overlays=Label(
+                    label="",
+                    style="margin: 0px 6px 0px 0px; font-size: 12px",  # to center the icon glyph
+                ),
+            ),
+        )
+
+        self.audio.connect("notify::speaker", self.on_speaker_changed)
+        self.event_box.connect("scroll-event", self.on_scroll)
+        self.add(self.event_box)
+
+    def on_scroll(self, _, event):
+        match event.direction:
+            case 0:
+                self.audio.speaker.volume += 8
+            case 1:
+                self.audio.speaker.volume -= 8
+        return
+
+    def on_speaker_changed(self, *_):
+        if not self.audio.speaker:
+            return
+        self.progress_bar.value = self.audio.speaker.volume / 100
+        self.audio.speaker.bind(
+            "volume", "value", self.progress_bar, lambda _, v: v / 100
+        )
+        return
+
 
 class StatusBar(Window):
-    def __init__(self, **kwargs):
+    def __init__(
+        self,
+    ):
         super().__init__(
+            name="bar",
             layer="top",
             anchor="left top right",
             exclusivity="auto",
-            **kwargs
+        )
+      
+        self.date_time = DateTime(
+            formatters=["%-I:%M %p %b %-d", "%-I:%M:%S %p %b %-d", "%a %-d %Y"],
+            name="datetime",
         )
 
-        self.date_time = DateTime(name="date-time")
+        self.system_tray = SystemTray(name="system-tray", spacing=4)
+
+        self.progress_bars_overlay = Overlay(
+            #child=
+            overlays=[
+            ],
+        )
+
+        self.status_container = Box(
+            name="widgets-container",
+            spacing=4,
+            orientation="h",
+            children=self.progress_bars_overlay,
+        )
+        self.status_container.add(VolumeWidget()) if AUDIO_WIDGET is True else None
 
         self.children = CenterBox(
             name="bar-inner",
@@ -22,7 +106,10 @@ class StatusBar(Window):
                 name="start-container",
                 spacing=4,
                 orientation="h",
-                children=[],
+                children=[
+                    self.date_time,
+                    self.status_container,
+                 ],
             ),
             center_children=Box(
                 name="center-container",
@@ -35,17 +122,22 @@ class StatusBar(Window):
                 spacing=4,
                 orientation="h",
                 children=[
-                    self.date_time,
                 ],
             ),
         )
 
+
+        self.show_all()
+
 def main():
     bar = StatusBar()
     app = Application("bar", bar)
+    app.set_stylesheet_from_file(get_relative_path("bar.css"))
+
     app.run()
 
 
 
 if __name__ == "__main__":
     main()
+
