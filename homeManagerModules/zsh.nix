eee8446c4af8777a74ff8450b76c79f38fd27b30
desiirenav <desiirenav@hotmail.com>
2025-07-08 02:19:52 -0400

    backuo

--- homeManagerModules/zsh.nix
+++ homeManagerModules/zsh.nix
@@ -6,5 +6,15 @@
 }: {
   programs.zsh = {
     enable = true;       
+    initContent = ''
+      if [ "$(tty)" = "/dev/tty1" ];then   
+         exec niri-session 
+      fi 
+    '';
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+      ];
+    };
   };
 }
